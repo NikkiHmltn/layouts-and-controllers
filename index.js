f93345ec9b31e72c27e1990295d40c710ce1a161
Nicole Hamilton <Nicole@Warps-MacBook-Air.local>
2020-11-16 13:08:02 -0800

    [views] add more routes for faves and dislikes foods and animals

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 
 app.get('/', (req, res) => {
-    res.send('hello from faves-and-hates')
+    res.render('index')
+})
+
+app.get('/faves/animals', (req, res) => {
+    const animals = ['peacock', 'flamingo', 'cats', 'unicorn']
+    res.render('favAnimals', { animals })
+})
+
+app.get('/faves/foods', (req, res) => {
+    const foods = ['pizza', 'spaghetti', 'bananas foster', 'green tea ice cream']
+    res.render('favFoods', { foods })
+})
+
+app.get('/dislikes/animals', (req, res) => {
+    const animals = ['spiders', 'snakes', 'poison dart frogs', 'crocodiles']
+    res.render('dislikesAnimals', { animals })
+})
+
+app.get('/dislikes/foods', (req, res) => {
+    const foods = ['olives', 'mushrooms', 'caviar', 'corned beef cabbage']
+    res.render('dislikesFoods', { foods })
 })
 
 app.listen(8000, () => {
